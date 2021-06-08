e009220ee36f84cef4ec033d3d4618befe8b708b
sophie <sophiefishman@gmail.com>
2021-06-08 09:30:35 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h1>{{ place.name }}</h1>
+      <h3>{{ place.address }}</h3>
+    </div>
+    <div>
+      <input type="text" v-model="newPlaceParams.name" placeholder="Name" />
+      <input type="text" v-model="newPlaceParams.address" placeholder="Address" />
+
+      <button v-on:click="createPlaces()">Create Place</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [],
+      newPlaceParams: {},
+      showPlace: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+
+    createPlaces: function () {
+      axios.post("http://localhost:3000/places", this.newPlaceParams).then((response) => {
+        console.log(response.data);
+        this.places.push(response.data);
+      });
+    },
+    updatePlace: function () {
+      axios.patch(`http://localhost:3000/places/${this.currentPlace.id}`, this.currentPlace).then((response) => {
+        console.log(response.data);
+        this.currentPlace = {};
+      });
+    },
+    destroyPlace: function () {
+      axios.delete(`http://localhost:3000/places/${this.currentPlace.id}`).then((response) => {
+        var index = this.places.indexOf(this.currentPlace);
+        this.places.splice(index, 1);
+        console.log(response.data);
+      });
+    },
   },
 };
 </script>
